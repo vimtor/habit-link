5d77a401d244ef0259f5c7bead09ae8866866f9f
Victor Navarro <victor@vimtor.io>
2021-07-14 18:01:30 +0200

    chore: hardhat config for frontend development

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,17 +1,31 @@
-import "@typechain/hardhat";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-ethers";
+import "hardhat-deploy-ethers";
+import "hardhat-deploy";
+import "@symfoni/hardhat-react";
+import "hardhat-typechain";
+import "@typechain/ethers-v5";
 import { task, HardhatUserConfig } from "hardhat/config";
 
 task("accounts", "Prints the list of accounts", async (args, hre) => {
     const accounts = await hre.ethers.getSigners();
-
     for (const account of accounts) {
         console.log(account.address);
     }
 });
 
 const config: HardhatUserConfig = {
+    react: {
+        providerPriority: ["web3modal", "hardhat"],
+    },
+    networks: {
+        hardhat: {
+            inject: false,
+            accounts: {
+                mnemonic: "test test test test test test test test test test test junk",
+            },
+        },
+    },
     solidity: "0.8.4",
 };
 
